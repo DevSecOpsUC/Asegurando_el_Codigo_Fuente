8bb69238e8600c5ece812760dc046d6c69bcaea4
AndresBarrios18 <pipe292011@gmail.com>
2025-11-06 15:00:12 -0500

    Primer commit

---
+++ Unidad1_AsegurandoCodigoFuente/Common/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common
+{
+    public static class StartupExtensions
+    {
+        public static void AddCommonServices(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+        }
+
+        public static void UseCommonPipeline(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseCors("AllowAll");
+        }
+    }
+}
---
+++ Unidad1_AsegurandoCodigoFuente/auth-service.Tests/UnitTest1.cs
@@ -0,0 +1,8 @@
+﻿using Xunit;
+
+namespace auth_service.Tests;
+public class UnitTest1
+{
+    [Fact]
+    public void Always_true() => Assert.True(true);
+}
